3c788ba76678eb81f398d316ad8c7c7d432e0498
Thiago <thiagolopezvega@gmail.com>
2023-08-03 13:15:53 -0300

    Desafio Numero 4

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import __dirname from './utils.js'
 import cartsRoutes from './routes/carts.router.js'
 import viewRouter from './routes/view.router.js'
 import { Server } from 'socket.io';
-import fs from "fs";
 
 const app = express();
 const PORT = 8080;
@@ -17,12 +16,8 @@ app.engine('handlebars', handlebars.engine());
 app.set('views', __dirname + '/views/');
 app.set('view engine', 'handlebars');
 
-
-
 app.use('/api/products', productsRoutes);
 app.use('/api/carts', cartsRoutes);
-
-
 
 const httpServer = app.listen(PORT, () => {
     console.log(`Server run on port: ${PORT}`);
@@ -35,16 +30,18 @@ const socketServer = new Server(httpServer);
 socketServer.on('connection', socket => {
     console.log("Nuevo usuario conectado");
 
-app.get('/', (req, res) => {
-fs.readFile('./files/Products.json', 'utf8', (err, data) => {
-    if (err) {
-    console.error('Error al leer el archivo de productos:', err);
-    res.status(500).send('Error al leer los datos de productos.');
-    return;
-}
-    const productos = JSON.parse(data);
-    res.render('home', { productos: productos });
-});
+app.get('/realtimeproducts', (req, res) => {
+    res.render('realTimeProducts', { productos: Products });
 });
 
-})
+app.post('/addproduct', (req, res) => {
+const newProduct = req.body;
+io.emit('productos', Products);
+res.redirect('/realtimeproducts');
+});
+
+io.on('connection', (socket) => {
+console.log('Usuario conectado');
+
+socket.emit('productos', Products);
+})});
--- src/routes/view.router.js
+++ src/routes/view.router.js
@@ -1,9 +1,48 @@
 import express from 'express';
 const router = express.Router();
+import path from 'path';
+import fs from 'fs';
+import __dirname from '../utils.js'
 
 
+const rutaArchivo = path.join(__dirname, '..', 'files', 'Products.json');
+const leerArchivoJSON = (ruta) => {
+    try {
+    const data = fs.readFileSync(ruta, 'utf8');
+    return JSON.parse(data);
+} catch (error) {
+    console.error('Error al leer el archivo JSON:', error);
+    return [];
+}
+};
+
+const guardarArchivoJSON = (ruta, data) => {
+    try {
+fs.writeFileSync(ruta, JSON.stringify(data, null, 2), 'utf8');
+console.log('Archivo JSON actualizado correctamente.');
+} catch (error) {
+    console.error('Error al guardar el archivo JSON:', error);
+    }
+};
+
+const Products = leerArchivoJSON(rutaArchivo);
 router.get('/', (req, res) => {
-    res.render('home', {})
-})
+    res.render('home', { Productos: Products });
+});
+
+router.get('/realtimeproducts', (req, res) => {
+    res.render('realTimeProducts', { productos: Products });
+});
+
+router.post('/addproduct', (req, res) => {
+const newProduct = req.body;
+
+const newProductId = Products.length + 1;
+newProduct.id = newProductId;
+Products.push(newProduct);
+guardarArchivoJSON(rutaArchivo, Products);
+res.redirect('/realtimeproducts');
+});
+
 
 export default router;
